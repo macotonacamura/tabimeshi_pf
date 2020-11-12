f777abb95689779da4e6a87d27eaf02f3cc5674f
vagrant <vagrant@localhost.localdomain>
2020-11-12 10:17:14 +0000

    adjust continent/

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,7 +1,7 @@
 class Country < ApplicationRecord
 	has_many :cities
 	has_many :reviews, through: :cities
-	belongs_to :continent
+	belongs_to :continent, optional: true
 
 	validates :country, presence: true
 end
