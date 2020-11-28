f6ef323129cce8436e83437a8f327db961304fd8
vagrant <vagrant@localhost.localdomain>
2020-11-28 09:24:35 +0000

    fixed thumbnail url

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,12 +2,12 @@ unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_REGION']
+      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+      region: ENV['S3_REGION']
     }
 
-    config.fog_directory  = ENV['AWS_S3_BUCKET_NAME']
+    config.fog_directory  = ENV['S3_BUCKET_NAME']
     config.cache_storage = :fog
   end
 end
