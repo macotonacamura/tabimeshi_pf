18863ba5e9d953c5d78169de3c91ce157e2bfe8f
vagrant <vagrant@localhost.localdomain>
2020-11-30 06:35:17 +0000

    changed IP address

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '13.113.0.158', user: 'ec2-user', roles: %w{app web db}
+server '18.176.35.14', user: 'ec2-user', roles: %w{app web db}
