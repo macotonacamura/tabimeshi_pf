dce8331cbe14973a2c962f66e0890388af07a4cd
vagrant <vagrant@localhost.localdomain>
2020-12-01 12:37:34 +0000

    add feature button to top

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -97,11 +97,31 @@ $(function(){
     });
 });
 
-
+//slick
 $(function(){
   $('.multiple-items').slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 3
+  });
+});
+
+
+
+$(function(){
+  var pagetop = $('#page_top');
+  // ボタン非表示
+  pagetop.hide();
+  // 100px スクロールしたらボタン表示
+  $(window).scroll(function () {
+     if ($(this).scrollTop() > 100) {
+          pagetop.fadeIn();
+     } else {
+          pagetop.fadeOut();
+     }
+  });
+  pagetop.click(function () {
+     $('body, html').animate({ scrollTop: 0 }, 500);
+     return false;
   });
 });
